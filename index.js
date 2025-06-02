2e42e4ca92f32530b19f5ef8468db873f3992f44
G.eight <bielalvenascimento@gmail.com>
2025-06-02 14:24:20 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express()
 const {conexao, closeConexao, testarConexao} = require('./src/DAO/conexao.js')
 const { produtod} = require('./src/DAO/produtos/produtod.js')
 const { pedidi} = require('./src/DAO/pedido/pedidi')
+const { buscarstatus } = require('./src/DAO/status/stattus.js')
+const { status } = require('express/lib/response.js')
+const { category } = require('./src/DAO/categoria/category.js')
+const { endereso } = require('./src/DAO/endereco/endereso.js')
 
 app.get('/empresalimpeza/v1', (req, res) => {
     let respInicial = {
@@ -27,6 +31,44 @@ app.get('/empresalimpeza/v1/pedido', async (req, res) =>{
     res.json(pedido)
 })
 
+app.get('/empresalimpeza/v1/categoria', async (req, res) =>{
+    let categoria = await category()
+    res.json(categoria)
+})
+
+app.get('/empresalimpeza/v1/status', async (req, res) =>{
+    let status = await stattus()
+    res.json(status)
+})
+
+
+app.get('/empresalimpeza/v1/endereco', async (req, res) =>{
+    let endereco = await endereso()
+    res.json(endereco)
+})
+
+app.get('/empresalimpeza/v1/itempedido', async (req, res) =>{
+    let itempedido = await intempedidi()
+    res.json(itempedido)
+})
+
+
+
+
+app.post('/empresalimpeza/v1/cliente', async (req, res) =>{
+    let clientes = await buscarClientes()
+    res.json(clientes)
+})
+
+app.post('/empresalimpeza/v1/produtos', async (req, res) =>{
+    let produtos = await produtod()
+    res.json(produtos)
+})
+
+app.post('/empresalimpeza/v1/pedido', async (req, res) =>{
+    let pedido = await pedidi()
+    res.json(pedido)
+})
 
 
 const porta = 3000
